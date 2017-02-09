ad0df0f0849533ebd2232736ae606557d12af133
erik <ebbaalhuis@gmail.com>
2017-02-09 19:14:00 +0100

    Main from template

--- src/hashcode_practice/main.java
+++ src/hashcode_practice/main.java
@@ -1,7 +1,94 @@
 package hashcode_practice;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 
 public class main {
-	public static void main(String[] args) {
-		System.out.println("HALLO");
+
+	// Input instances
+	public static String[] instances = { "Example", "Small", "Medium", "Big" };
+
+	// variables that are read from input
+	public static long nRow, nCol, deadline, maxLoad, totalScore;
+	public static int nType, nWarehouse, nOrders, nDrones, nCommands;
+	public static long[] weight, available;
+
+	// variables for making output
+	public static BufferedWriter bw;
+
+	public static void main(String[] args) throws IOException {
+		for (String s : instances) {
+			String inputDir = "/home/git/hashcode_practice/files/" + s + ".in";
+			String outDir = "/home/git/hashcode_practice/files/" + s + ".out";
+			readInput(inputDir);
+			initWriter(outDir);
+
+			// Solution method here
+			sol1(outDir);
+
+			bw.close();
+			
+			// Evaluate solution locally, if needed
+			long result = evaluateSolution(outDir);
+			if (!s.equals("example")) {
+				totalScore += result;
+			}
+			System.err.printf("Result for instance %s: ", s);
+			System.err.println(result);
+		}
+
+		System.err.printf("Total score: %d\n", totalScore);
 	}
+
+	public static void readInput(String fileDir) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileDir));
+		String[] split;
+
+		// Read input here
+
+		br.close();
+	}
+	
+	public static void sol1(String fileDir) throws IOException {
+		// Solution method goes here
+	}
+
+	public static long evaluateSolution(String fileDir) {
+		// Client-side evaluation of solution
+		return 42;
+	}
+	
+	public static void writeLine(String s) {
+		try {
+			bw.write(s);
+			bw.write("\n");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void filePrepend(String line, String fileDir) throws IOException {
+		String tempDir = "/home/erik/Hashcode2016/temp";
+		Files.copy(Paths.get(fileDir), Paths.get(tempDir), StandardCopyOption.REPLACE_EXISTING);
+		initWriter(fileDir);
+		bw.write(line);
+		BufferedReader br = new BufferedReader(new FileReader(tempDir));
+		String tempLine = "";
+		while ((tempLine = br.readLine()) != null) {
+			bw.write(tempLine);
+			bw.write("\n");
+		}
+		br.close();
+		bw.close();
+	}
+
+	public static void initWriter(String fileDir) throws UnsupportedEncodingException, FileNotFoundException {
+		bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileDir), "utf-8"));
+	}
+
+
 }

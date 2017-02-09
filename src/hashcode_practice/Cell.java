61e0a14a861b98e7aa9d6e36c3ef96856ba0dd72
erik <ebbaalhuis@gmail.com>
2017-02-09 20:16:28 +0100

    zz

---
+++ src/hashcode_practice/Cell.java
@@ -0,0 +1,13 @@
+package hashcode_practice;
+
+public class Cell {
+	public int id, r, c;
+	public boolean isT;
+	
+	Cell(int _id, int _r, int _c, boolean _t) {
+		id = _id;
+		r = _r;
+		c = _c;
+		isT = _t;
+	}
+}
---
+++ src/hashcode_practice/Slice.java
@@ -0,0 +1,34 @@
+package hashcode_practice;
+
+import java.util.ArrayList;
+
+public class Slice {
+	public int id;
+	public ArrayList<Cell> cells;
+	
+	Slice(int _id) {
+		id = _id;
+		cells = new ArrayList<>();
+	}
+	
+	public void addCell(Cell c) {
+		cells.add(c);
+	}
+	
+	public void removeCell(Cell c) {
+		cells.remove(c);
+	}
+	
+	public boolean isLegal(int req, int maxCells) {
+		if (maxCells < cells.size()) {
+			return false;
+		}
+		int tCount = 0;
+		for (Cell c : cells) {
+			if (c.isT) {
+				tCount++;
+			}
+		}
+		return (tCount >= req && cells.size()-tCount >= req);
+	}
+}
